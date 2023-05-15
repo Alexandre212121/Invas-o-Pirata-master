9a398176a25af89d07f1196d2bc77e50e8e8d9e8
Alexandre212121 <al2311alexandrepinheiro@gamil.com>
2023-05-15 20:12:58 -0300

    a

---
+++ js/ground.js
@@ -0,0 +1,26 @@
+class Ground{
+    constructor(x,y,w,h, angle, color){
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.angle = angle
+        this.color = color
+        this.ground = Bodies.rectangle(this.x, this.y, this.w, this.h,{isStatic:true})
+        World.add(world, this.ground);
+    }
+
+    display(){
+        Body.rotate(this.ground, this.angle)
+        rectMode(CENTER)
+
+        push()
+        translate(this.x, this.y)
+        rotate(this.angle)
+        fill(this.color)
+        rect(0, 0, this.w, this.h)
+        pop()
+
+        this.angle += 0.1
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,42 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+
+var engine, world
+
+var ground, bg
+
+var tower, cannon
+
+var angle = 50
+
+function preload() {
+  bg = loadImage("./assets/background.gif")
+}
+
+function setup() {
+
+  angleMode(DEGREES)
+
+  canvas = createCanvas(1200, 600);
+  engine = Engine.create();
+  world = engine.world;
+
+  tower = new Tower(150, 350, 160, 310)
+  ground = new Ground(100, 400, 100, 20)
+  cannon = new Cannon(180, 110, 110, 70, angle)
+
+
+}
+
+function draw() {
+  background(189);
+  image(bg, 0, 0, width, height)
+  tower.display()
+  cannon.display()
+  strokeWeight(2)
+  textAlign(CENTER)
+  text("X: " + mouseX + " / " + "Y: " + mouseY, mouseX, mouseY)
+}
